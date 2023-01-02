5b358d89acf23b8dec74c5f5c8abea104c095ed6
barkstone <barkstone2@gmail.com>
2023-01-02 17:01:54 +0900

    feat: Add list parser for both ordered, unordered

---
+++ src/main/java/html/parser/ListHtmlParser.kt
@@ -0,0 +1,47 @@
+package html.parser
+
+import common.Util
+import org.jsoup.nodes.Element
+
+class ListHtmlParser : HtmlParser, ParentNode() {
+
+    private val listTagMap = mapOf(
+        Pair("bulleted_list_item", "ul"),
+        Pair("numbered_list_item", "ol"),
+    )
+    private val tistoryListTypeMap = mapOf(
+        Pair("ul", "disc"),
+        Pair("ol", "decimal")
+    )
+
+    override fun parse(block: Map<String, Any>): Element? {
+        val listType = block["type"] as String
+        val tagName = listTagMap[listType]
+        val listInfo = block[listType] as Map<String, Any>
+
+        val richTexts = listInfo["rich_text"] as List<Map<String, Any>>
+
+        var listWrapTag = Element(tagName)
+            .attr("data-ke-list-type", tistoryListTypeMap[tagName])
+            .attr("style",
+                buildString {
+                    append("list-style-type: ")
+                        .append(tistoryListTypeMap[tagName])
+                        .append(";")
+                })
+
+
+        var listItemTag = Element("li")
+
+        for (richText in richTexts) {
+            val textNodes = Util.paragraphHtmlParser.createTextNodes(richText)
+            listItemTag.appendChildren(textNodes)
+        }
+
+        appendChildIfExist(block, listItemTag)
+
+        listWrapTag.appendChild(listItemTag)
+
+        return listWrapTag
+    }
+}
