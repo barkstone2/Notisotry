0a828348ddaa89adfcb994eb73442e080753662d
barkstone <barkstone2@gmail.com>
2023-01-02 17:02:26 +0900

    feat: Add quote parser

---
+++ src/main/java/html/parser/QuoteHtmlParser.kt
@@ -0,0 +1,23 @@
+package html.parser
+
+import common.Util
+import org.jsoup.nodes.Element
+import org.jsoup.nodes.Node
+
+class QuoteHtmlParser : HtmlParser {
+
+    override fun parse(block: Map<String, Any>): Node? {
+        val quoteInfo = block["quote"] as Map<String, Any>
+        val richTexts = quoteInfo["rich_text"] as List<Map<String, Any>>
+
+        val blockquote = Element("blockquote")
+        blockquote.attr("data-ke-style", "style2")
+
+        for (richText in richTexts) {
+            val textNodes = Util.paragraphHtmlParser.createTextNodes(richText)
+            blockquote.appendChildren(textNodes)
+        }
+
+        return blockquote
+    }
+}
