f7a5ee65636e44d65ac7dfeb6f58ba2353e53443
barkstone <barkstone2@gmail.com>
2023-01-02 17:20:03 +0900

    feat: Add attributes to element for tistory tag parsing

--- src/main/java/html/parser/TableHtmlParser.kt
+++ src/main/java/html/parser/TableHtmlParser.kt
@@ -7,6 +7,8 @@ open class TableHtmlParser : HtmlParser, ParentNode() {
 
     override fun parse(block: Map<String, Any>): Node {
         val table = Element("table")
+        table.attr("data-ke-align", "alignLeft")
+
         val tbody = Element("tbody")
         appendChildIfExist(block, tbody)
         table.appendChild(tbody)
