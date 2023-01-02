633a44d0ea5f67caf14a51ab1218d261746160ea
barkstone <barkstone2@gmail.com>
2023-01-02 18:03:22 +0900

    feat: Add notion url const for creating notion api url

--- src/main/java/notion/api/NotionApiController.kt
+++ src/main/java/notion/api/NotionApiController.kt
@@ -5,6 +5,19 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import common.Util
 import java.io.File
 
+private const val BASE_URL = "https://api.notion.com/v1"
+
+private const val DATABASE_URL_PREFIX = "databases"
+private const val DATABASE_URL_SUFFIX = "query"
+
+private const val BLOCK_URL_PREFIX = "blocks"
+private const val BLOCK_URL_SUFFIX = "children"
+
+private const val NOTION_VERSION_HEADER_KEY = "Notion-Version"
+private const val NOTION_VERSION_HEADER_VALUE = "2022-06-28"
+
+private const val AUTHORIZATION_HEADER_KEY = "Authorization"
+
 class NotionApiController {
     private var databaseId: String? = null
     private var databaseOptions: HashMap<String, Any> = HashMap()
